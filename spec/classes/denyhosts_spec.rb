3aa222d8aaf1c727e5efe3fa92a9a8fb8ab25322
Martin Jackson <martin@uncommonsense-uk.com>
2013-06-21 11:52:34 +0100

    Added rspec tests and .gitignore. Fixes #1.

---
+++ spec/classes/denyhosts_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+describe 'denyhosts', :type => :class do
+
+  describe 'when deploying on debian' do
+    let :facts do
+      {
+          :osfamily        => 'Debian',
+          :operatingsystem => 'Debian',
+          :lsbdistcodename => 'sid',
+      }
+    end
+
+    describe 'by default' do
+      it { should contain_package('denyhosts').with({
+            :ensure => 'installed'
+           })
+      }
+      it { should contain_file('/etc/denyhosts.conf').with({
+            :owner   => 'root',
+            :group   => 'root',
+            :mode    => '644',
+            :notify  => 'Service[denyhosts]'
+           })
+      }
+      it { should contain_file('/etc/denyhosts.conf').with_content(/ADMIN_EMAIL\s=\sroot@localhost/) }
+      it { should contain_service('denyhosts').with({
+            :ensure    => 'running',
+            :enable    => 'true',
+            :hasstatus => 'false',
+            :pattern   => 'denyhosts',
+            :require   => 'Package[denyhosts]'
+           })
+      }
+      it { should contain_file('/etc/hosts.allow').with({
+            :owner   => 'root',
+            :group   => 'root',
+            :mode    => '644'
+           })
+      }
+    end
+
+    describe 'With no email address and 3 hosts defined' do
+      let :params do
+        {
+            :adminemail => '',
+            :allow      => ['host1','host2','host3'],
+        }
+      end
+      it { should contain_file('/etc/denyhosts.conf').with_content(/ADMIN_EMAIL\s=\s+$/) }
+      it { should contain_file('/etc/hosts.allow').with_content(/host1\n/) }
+      it { should contain_file('/etc/hosts.allow').with_content(/host2\n/) }
+      it { should contain_file('/etc/hosts.allow').with_content(/host3\n/) }
+    end
+  end
+end
